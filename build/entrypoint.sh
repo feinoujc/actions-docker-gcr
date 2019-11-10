b6bd1a0f7fbfc47edfe0f0885f8cc6cd9f62fe64
Joe Feinour <1733707+feinoujc@users.noreply.github.com>
2019-11-10 16:48:32 -0500

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -11,6 +11,6 @@ docker build -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$TAG
 if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
   BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
-  BRANCH_TAG=$BRANCH_$(echo $TAG | cut -c1-7)
+  BRANCH_TAG=${BRANCH}_$(echo $TAG | cut -c1-7)
   docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$BRANCH_TAG
 fi
